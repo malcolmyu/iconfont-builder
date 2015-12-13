9744d058570d1cfe8ed4f5e91849efa73735ec35
malcolm <minghao_yu@163.com>
2015-12-13 23:48:19 +0800

    add test case

--- test/builder/_index.js
+++ test/builder/_index.js
@@ -7,43 +7,79 @@ var _ = require('underscore');
 var builder = require('../../lib');
 
 var basePath = 'test/builder';
+var dest = path.join(basePath, 'dest');
+
+function getOptions() {
+  return {
+    icons: [
+      {
+        name: 'www-font-o',
+        codepoint: 0xF001,
+        file: 'test.svg'
+      }
+    ],
+    src: basePath,
+    dest: dest,
+    fontName: 'iconfont'
+  };
+}
+
+function generateFonts(options) {
+  var hasDirQ = Q.nfcall(fs.access, dest);
+  var rmDirQ = Q.nfcall(fs.rmdir, dest);
+
+  return hasDirQ
+    .then(rmDirQ)
+    .finally(function() {
+      return builder(options);
+    })
+    .then(function() {
+      var fonts = ['svg', 'ttf', 'eot', 'woff'];
+      var fontsQ = _.map(fonts, function(type) {
+        var file = path.join(dest, 'iconfont.' + type);
+        return Q.nfcall(fs.access, file);
+      });
+
+      return Promise.all(fontsQ)
+    });
+}
 
 describe('生成正确的字体文件', function() {
   it('传递完整的icon信息', function(done) {
-    var dest = path.join(basePath, 'dest');
-    var options = {
-      icons: [
-        {
-          name: 'www-font-o',
-          codepoint: 0xF001,
-          file: 'test.svg'
-        }
-      ],
-      src: basePath,
-      dest: dest,
-      fontName: 'iconfont'
-    };
+    var options = getOptions();
 
-    var hasDirQ = Q.nfcall(fs.access, dest);
-    var rmDirQ = Q.nfcall(fs.rmdir, dest);
+    generateFonts(options)
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
 
-    hasDirQ.then(rmDirQ).finally(function() {
-      builder(options)
-        .then(function() {
-          var fonts = ['svg', 'ttf', 'eot', 'woff'];
-          var fontsQ = _.map(fonts, function(type) {
-            var file = path.join(dest, 'iconfont.' + type);
-            return Q.nfcall(fs.access, file);
-          });
+  it('缺失部分icon信息', function(done) {
+    var options = getOptions();
+    delete options.icons[0].name;
 
-          return Promise.all(fontsQ)
-        })
-        .then(function() {
-          done()
-        })
-        .catch(function(err) {
-          done(err);
-        });
-    });
-  })
+    generateFonts(options)
+      .then(function() {
+        done(new Error('It cannot be resolved'))
+      })
+      .catch(function() {
+        done();
+      });
+  });
+
+  it('不传递icon信息', function(done) {
+    var options = getOptions();
+    delete options.icons;
+
+    generateFonts(options)
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
 });
